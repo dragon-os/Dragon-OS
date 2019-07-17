f021b2c7e104ba3718fc0de291f9a4dc280d6976
astronomize <astronomizestudios@gmail.com>
2019-07-17 16:21:51 -0600

    Initial commit

---
+++ Kernel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using drivers.display;
+using drivers.file;
+
+using Cosmos.Core.IOGroup;
+using Cosmos.Common;
+using Sys = Cosmos.System;
+using Cosmos.System.FileSystem;
+
+namespace OS
+{
+    /*
+     * Kernel.cs
+     * 
+     * The operating system kernal.
+    */
+    public class Kernel : Sys.Kernel
+    {
+        // -------------------- OS VARIABLES --------------------
+        String OS_NAME = "OS"; // the name of the operating system.
+        String VERSION = "0.00.00"; // the version of the operating system.
+
+        // -------------------- DRIVERS --------------------
+        private static DisplayDriver displayDriver; // the display driver.
+        private static Mouse mouse; // the mouse driver.
+        private static FAT fat; // the file system driver.
+
+        protected override void BeforeRun()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(OS_NAME + " bootloader/kernel v" + VERSION + " has loaded. ");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(OS_NAME + " loading FAT drivers...");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            fat = new FAT();
+
+            CosmosVFS fs = null;
+            
+            // check the amount of FAT filesystems.
+            if(fs.GetVolumes().Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No FAT file system found!");
+                Console.ForegroundColor = ConsoleColor.White;
+            } else
+            {
+                fs = new Sys.FileSystem.CosmosVFS();
+                Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("FAT file system found!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+
+        protected override void Run()
+        {
+        }
+    }
+}
---
+++ drivers/FAT.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Sys = Cosmos.System;
+using Cosmos.Common;
+using Cosmos.System.FileSystem;
+using System.IO;
+
+namespace drivers.file
+{
+    class FAT
+    {
+        CosmosVFS fs = new Sys.FileSystem.CosmosVFS();            
+
+        public string[] ReadLines(string FileAdr) //Returns the lines of text in string[].
+        {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            string[] FileRead;
+            FileRead = File.ReadAllLines(FileAdr);
+            return FileRead;
+        }
+
+        public string ReadText(string FileAddr) //Returns the file in a single string.
+        {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            string f_contents = "";
+            f_contents = File.ReadAllText(FileAddr);
+            return f_contents;
+        }
+
+        public byte[] ReadByte(string FileAdr) //Returns the read file in bytes.
+        {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            byte[] FileRead;
+            FileRead = File.ReadAllBytes(FileAdr);
+            return FileRead;
+        }
+
+        public string[] GetFromAddr(string Adr) // Get Files From Address
+        {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            string[] Files = new string[256];
+            if (Directory.GetFiles(Adr).Length > 0)
+                Files = Directory.GetFiles(Adr);
+            else
+                Files[0] = "No files found.";
+            return Files;
+        }
+        public string[] GetDirsFromAddress(string Adr) // Get Directories From Address
+        {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            var Dirs = Directory.GetDirectories(Adr);
+            return Dirs;
+        }
+    }
+}
